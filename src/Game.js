f0e009bd9d0d7ca08734bccb31725910aa65376b
Lee Jia Sheng <thegreyjs@gmail.com>
2022-06-12 02:39:35 +0800

    Create Game.js

---
+++ src/Game.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const helmet = ['w', 'r', 'y', 'b', 't'];
+const visor = ['w', 'r', 'y', 'b', 't'];
+const suit = ['w', 'r', 'y', 'b', 't'];
+const gloves = ['w', 'r', 'y', 'b', 't'];
+const boots = ['w', 'r', 'y', 'b', 't'];
+
+class Game extends React.Component {
+
+}
