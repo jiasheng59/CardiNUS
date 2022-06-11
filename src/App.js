6e8a7b8b0fd01c4fad5b5319c578611578cd4c8e
Lee Jia Sheng <thegreyjs@gmail.com>
2022-06-11 21:15:16 +0800

    Testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import "./App.css";
 import LoginPage from "./LoginPage";
 
 const App = () => {
-  return <LoginPage></LoginPage>
+  return (
+    <div>
+      <p>Please Login / Sign Up.</p>
+      <LoginPage></LoginPage>
+    </div>
+  );
 };
 
 export default App;
