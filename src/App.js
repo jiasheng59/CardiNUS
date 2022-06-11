8113b6167fb8fa69ec8a090f098e4b960f8f7bd8
Lee Jia Sheng <thegreyjs@gmail.com>
2022-06-11 21:37:59 +0800

    Move the login page to the file LoginPage.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import fire from "./fire";
-import Login from "./Login";
-import Hero from "./Hero";
 import "./App.css";
 import LoginPage from "./LoginPage";
 
